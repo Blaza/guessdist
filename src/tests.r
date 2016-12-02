561f4793c16447bc78dce4eafa01d89b87019bea
Blaza <radebeka@gmail.com>
2016-12-03 00:35:32 +0100

    Add tests.r
    This file contains test methods which will be used for goodness of fit

---
+++ src/tests.r
@@ -0,0 +1,72 @@
+library(goftest)
+library(KScorrect)
+library(fitdistrplus)
+
+# We will work with objects of class fitdist from fitdistrplus
+# test class interface:
+#   name='long name'
+#   code='shorthand id'
+#   warning='warning message regarding this test'
+#   fun=function(fit){return(pval)}
+
+tests <- list(
+              list(
+                   name='Chi-squared',
+                   code='chisq',
+                   warning='',
+                   fun=function(fit){
+                       gof <- gofstat(fit)
+                       return(gof$chisqpvalue)
+                   }
+                  ),
+              list(
+                   name='Kolmogorov-Smirnov',
+                   code='ks',
+                   warning='K-S test may give very inaccurate p-values, because
+                            the parameters are estimated. This should be
+                            possible to correct using the Lilliefors-Corrected
+                            version of the test, but for a limited number of
+                            distributions.',
+                   fun=function(fit){
+                   }
+                  ),
+              list(
+                   name='Lilliefors-Corrected Kolmogorov-Smirnov',
+                   code='lcks',
+                   warning='Lilliefors-Corrected K-S test can only be applied
+                            to normal, log-normal, uniform, exponential, gamma
+                            and Weibull distributions.',
+                   fun=function(fit){
+                   }
+                  ),
+              list(
+                   name='Anderson-Darling',
+                   code='ad',
+                   warning='Anderson-Darling test may give inaccurate p-values
+                            as the parameters are estimated. For a limited
+                            number of distributions, Lilliefors-Corrected
+                            Kolmogorov-Smirnov test may give better results.',
+                   fun=function(fit){
+                       pdf.name <- paste('p', fit$distname, sep='')
+                       args <- append(list(fit$data, pdf.name),
+                                      as.list(fit$estimate))
+                       test <- do.call('ad.test', args)
+                       return(test$p.value)
+                   }
+                  ),
+              list(
+                   name='Cramer-von Mises',
+                   code='cvm',
+                   warning='Cramer-von Mises test may give inaccurate p-values
+                            as the parameters are estimated. For a limited
+                            number of distributions, Lilliefors-Corrected
+                            Kolmogorov-Smirnov test may give better results.',
+                   fun=function(fit){
+                       pdf.name <- paste('p', fit$distname, sep='')
+                       args <- append(list(fit$data, pdf.name),
+                                      as.list(fit$estimate))
+                       test <- do.call('cvm.test', args)
+                       return(test$p.value)
+                   }
+                  ),
+             )
